bc8a57f7b83f9de67828531b3e50d37b16529ae3
Ro Lee <rolee0429@gmail.com>
2020-09-06 23:18:35 +0900

    FS-29: Change Artifact

--- src/main/java/com/rostradamus/fsarcheureka/FsArchEurekaApplication.java
+++ src/main/java/com/rostradamus/fsarcheureka/FsArchEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.rostradamus.fsarcheureka;
+package com.fitsight.fsarcheureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rostradamus/fsarcheureka/FsArchEurekaApplicationTests.java
+++ src/test/java/com/rostradamus/fsarcheureka/FsArchEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rostradamus.fsarcheureka;
+package com.fitsight.fsarcheureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
