1f3657e450ce4d2810d7d46af1fc88ee7c73cbf1
Ro Lee <rolee0429@gmail.com>
2020-09-06 22:52:54 +0900

    FS-29: Implement Microservice Structure

---
+++ src/test/java/com/rostradamus/fsarcheureka/FsArchEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostradamus.fsarcheureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsArchEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
